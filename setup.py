a71e05ab60a99c459c9e26d20a6b19d113eac1d2
polo le cool <84383048+popolecool@users.noreply.github.com>
2023-02-03 08:46:49 +0100

    mis a jour pour cloude flair

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import subprocess
+    #pour python3
+# We load the newline seperated commands from the commands file. 
+    #with open("commands.txt", "r") as fh:
+    # We use the readlines() method to get a list of all the lines in the file.
+#    cmds = fh.readlines()
+#    for item in cmds:
+    # We run each command using the subprocess.run() function in Python.
+#        subprocess.run(item.split(" "))
 
-# We load the newline seperated commands from the commands file.
 with open("commands.txt", "r") as fh:
-# We use the readlines() method to get a list of all the lines in the file.
     cmds = fh.readlines()
     for item in cmds:
-# We run each command using the subprocess.run() function in Python.
-        subprocess.run(item.split(" "))
+    subprocess.call(item.split(" "))
